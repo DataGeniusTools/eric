bea51198a3f5bc386f93b0751bbde82a07c2d606
Markus Kolp <markus.kolp-extern@messe-muenchen.de>
2024-04-04 09:04:47 +0200

    reduced minheight of node for entity w/o attribute

--- src/CustomNode.js
+++ src/CustomNode.js
@@ -4,7 +4,7 @@ import { NumberOutlined, KeyOutlined, ClockCircleOutlined, FontSizeOutlined} fro
 
 const CustomNode = ({ data }) => {
   return (
-    <div className="custom-node" style={{ color: 'rgb(15, 23, 42)', border: '1px solid #ddd', borderRadius: '4px', padding: '0px', minWidth: '200px', minHeight: '60px', background: '#fff' }}>
+    <div className="custom-node" style={{ color: 'rgb(15, 23, 42)', border: '1px solid #ddd', borderRadius: '4px', padding: '0px', minWidth: '200px', minHeight: '20px', background: '#fff' }}>
       <div className="title" style={{ fontWeight: '600', backgroundColor: '#eee', textAlign: 'left', marginBottom: '0px', paddingLeft: '10px', paddingRight: '10px', paddingTop: '5px', paddingBottom: '5px' }}>
         {data.title}
       </div>
